47d9d97c916f7d9d5d02c797cb17c49f1544374e
Pedro <149411192+phpBass@users.noreply.github.com>
2025-04-29 09:13:24 -0300

    Update urls.py

--- projeto_doacoes/projeto_doacoes/urls.py
+++ projeto_doacoes/projeto_doacoes/urls.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
-from doacoes.views import login_view, home  # Corrigido aqui
+from doacoes.views import home  # Apenas home aqui
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('doacoes/', include('doacoes.urls')),
-    path('login/', login_view, name='login'),
-    path('', home, name='home'),  # Corrigido aqui
-]
-
-urlpatterns += [
+    path('doacoes/', include('doacoes.urls')),  # Inclui rotas específicas do app
+    path('', home, name='home'),  # Página principal
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
