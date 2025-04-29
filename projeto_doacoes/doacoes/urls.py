2d9ce0e10bb49b1bacb2ed10efd14b54803ff988
Pedro <149411192+phpBass@users.noreply.github.com>
2025-04-29 09:26:17 -0300

    Update urls.py

--- projeto_doacoes/doacoes/urls.py
+++ projeto_doacoes/doacoes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import login_view, listar_doacoes, adicionar_doacao, registro_view, home
+from .views import login_view, listar_doacoes, adicionar_doacao, registro_view, #home
 
 urlpatterns = [
     path("login/", login_view, name="login"),  # /doacoes/login/
     path("registro/", registro_view, name="registro"),
     path("", listar_doacoes, name="listar_doacoes"),  # /doacoes/
     path("adicionar/", adicionar_doacao, name="adicionar_doacao"),
-    path("home/", home, name="home"),  # /doacoes/home/
+#    path("home/", home, name="home"),  # /doacoes/home/
 ]
